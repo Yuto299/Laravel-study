03f1b555800d963e45f385a1b2d19fc91d3414b5
ota.se.ke@outlook.jp <user@LAPTOP-5B2IV824.>
2025-01-15 11:59:29 +0900

    ページ移行完了

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,27 @@
 <?php
 
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\UnilityController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/hello-world', fn() => 'hello world');
+Route::get('/hello-world', fn() => view('hello_world'));
+Route::get('/hello', fn() => view('hello', [
+    'name' => '山田',
+    'course' => 'Laravel'
+]));
+
+Route::get('/', fn() => view('index'));
+Route::get('/curriculum', fn() => view('curriculum'));
+
+// 世界の時間
+Route::get('/world-time', [UnilityController::class, 'worldTime']);
+
+// おみくじ
+Route::get('/omikuji', [GameController::class, 'omikuji']);
+
+// モンティ・ホール問題
+Route::get('/monty-hall', [GameController::class, 'montyHall']);
